7bc9286014896e39cb3ac964b1b44ac5342f685b
Adrian Koch <81982963+jizyo@users.noreply.github.com>
2024-11-03 12:24:45 +0100

    added packages and dependencies

--- Rebuilding-Guilded-Rose/src/main/java/com/guildedrose/Main.java
+++ Rebuilding-Guilded-Rose/src/main/java/com/guildedrose/Main.java
@@ -2,6 +2,6 @@ package com.guildedrose;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to Guilded Rose!");
     }
 }
